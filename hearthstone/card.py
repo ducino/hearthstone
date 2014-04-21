d132bcc7ba0f2b0f70a8857e95e6a8b733150888
chinesedfan <chinesedfan@qq.com>
2014-04-21 21:34:43 +0800

    update Card structure

--- hearthstone/card.py
+++ hearthstone/card.py
@@ -1,17 +1,40 @@
 from __future__ import print_function
 
 class Card(object):
+	'''
+	Sample:
+	{
+        "collectible": 1, 
+        "set": 3,     # Invalid, Test_Temporary, Core, Expert1, Reward, Missions, Demo, None, Cheat, Blank, Debug_SP, Promo, FP1, PE1, FP2, PE2, Credits
+        "name": "Edwin VanCleef", 
+        "faction": 3, # Invalid, Horde, Alliance, Neutral
+        "elite": 1,   # as well as Legendary
+        "quality": 5, # Invalid, Free, Common, Rare, Epic, Legendary
+        "image": "EX1_613", 
+        "description": "Combo: Gain +2/+2 for each card played earlier this turn.", 
+        "classs": 4,  # Invalid, Warrior=1, Paladin, Hunter, Rogue, Priest, Warlock=7, Shaman, Mage, Druid=11 
+        "attack": 2, 
+        "cost": 3, 
+        #"race": 0,   # Invalid, BloodElf, Draenei, Dwarf, Gnome, Goblin, Human, NightElf, Orc, Tauren, Troll, Undead, Worgen, Goblin2, Murloc, Demon, Scourge, Mechanical, Elemental, Ogre, Pet, Totem, Nerubian, Pirate, Dragon 
+        "health": 2, 
+        "popularity": 749, 
+        "type": 4,    # Invalid, Game, Player, Hero, Minion, Ability, Enchantment, Weapon, Item, Token, Hero_Power 
+        "id": 306, 
+        "icon": "inv_misc_ticket_tarot_beasts_01"
+        }	
+	'''
 	def __init__(self, data):
 		self.name = data["name"]
-		self.mana = data["mana"]
-		self.attack = data["attack"]
-		self.health = data["health"]
+		self.mana = data["cost"]
+		self.attack = _get_field(data, "attack", 0)
+		self.health = _get_field(data, "health", 0)
 		self.type = data["type"]
-		self.race = data["race"]
-		self.cls = data["class"]
+		self.race = _get_field(data, "race", 0)
+		self.cls = _get_field(data, "classs", 0)
 		self.set = data["set"]
 		self.quality = data["quality"]
-		self.desc = data["description"]
+		self.desc = _get_field(data, "description", "")
+		self.image = data["image"]
 	
 	def __str__(self):
 		text = []
@@ -29,6 +52,9 @@ class Card(object):
 			   _contains(self.type, keyword) or\
 			   _contains(self.race, keyword) or\
 			   _contains(self.desc, keyword)
+
+def _get_field(data, field, default):
+	return data[field] if field in data else default
 	
 def _contains(sentence, word):
 	return sentence.lower().find(word.lower()) != -1
